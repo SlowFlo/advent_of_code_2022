119f1ee6d397582bf817e73a7b8298b8b1e51614
SlowFlo <fbautry@gmail.com>
2022-12-06 13:37:20 +0100

    feat: gestion de plusieurs inventaires + refacto

--- Day 1 - Calorie Counting/most_calories.py
+++ Day 1 - Calorie Counting/most_calories.py
@@ -1,7 +1,18 @@
-def most_calories(input: str):
-    if not input:
+import re
+
+
+def sum_calories(calories: str) -> int:
+    if not calories:
         return 0
 
-    cleared_list = [int(calorie_str) for calorie_str in input.split("\n") if calorie_str.strip()]
+    cleared_list = [int(calorie_str) for calorie_str in calories.split("\n") if calorie_str.strip()]
 
     return sum(cleared_list)
+
+
+def most_calories(input: str):
+
+    elves_regex = re.compile(r"\n\s*\n")
+    calories_by_elves = elves_regex.split(input)
+
+    return max(map(sum_calories, calories_by_elves))
--- Day 1 - Calorie Counting/test_most_calories.py
+++ Day 1 - Calorie Counting/test_most_calories.py
@@ -1,20 +1,20 @@
-from most_calories import most_calories
+from most_calories import most_calories, sum_calories
 
 
 def test_empty_str_is_0():
     input_str = ""
-    assert most_calories(input_str) == 0
+    assert sum_calories(input_str) == 0
 
 
 def test_simple_str_is_converted_to_int():
     input_str = "100"
-    assert most_calories(input_str) == 100
+    assert sum_calories(input_str) == 100
 
 
 def test_multiline_str_is_additionned():
     input_str = """100
         200"""
-    assert most_calories(input_str) == 300
+    assert sum_calories(input_str) == 300
 
 
 def test_empty_or_blank_lines_are_ok():
@@ -22,4 +22,15 @@ def test_empty_or_blank_lines_are_ok():
         100
         200
         """
-    assert most_calories(input_str) == 300
+    assert sum_calories(input_str) == 300
+
+
+def test_several_inventories_return_the_max():
+    input_str = """
+        100
+        200
+        
+        300
+        400
+        """
+    assert most_calories(input_str) == 700
