a471493046f08beeaa67bc0473da6511891ba685
SlowFlo <fbautry@gmail.com>
2022-12-10 21:42:02 +0100

    feat: ajout de properties

--- Day 3 - Rucksack Reorganization/partie 1/rucksack.py
+++ Day 3 - Rucksack Reorganization/partie 1/rucksack.py
@@ -1,5 +1,21 @@
 class Rucksack:
 
     def __init__(self, items: str):
-        self.first_compartement = items[:len(items)//2]
-        self.second_compartement = items[len(items)//2:]
+        self.first_compartment = items[:len(items)//2]
+        self.second_compartment = items[len(items)//2:]
+
+    @property
+    def first_compartment(self) -> str:
+        return self._first_compartment
+
+    @first_compartment.setter
+    def first_compartment(self, value: str):
+        self._first_compartment = value
+
+    @property
+    def second_compartment(self) -> str:
+        return self._second_compartment
+
+    @second_compartment.setter
+    def second_compartment(self, value: str):
+        self._second_compartment = value
--- Day 3 - Rucksack Reorganization/partie 1/test_rucksack.py
+++ Day 3 - Rucksack Reorganization/partie 1/test_rucksack.py
@@ -3,5 +3,5 @@ from rucksack import Rucksack
 
 def test_get_compartments_content():
     my_rucksack = Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
-    assert my_rucksack.first_compartement == "vJrwpWtwJgWr"
-    assert my_rucksack.second_compartement == "hcsFMMfFFhFp"
+    assert my_rucksack.first_compartment == "vJrwpWtwJgWr"
+    assert my_rucksack.second_compartment == "hcsFMMfFFhFp"
