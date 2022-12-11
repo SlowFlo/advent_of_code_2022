bf20d738e3857b4a792610db7c8dbc0df7938ea8
SlowFlo <fbautry@gmail.com>
2022-12-11 12:41:21 +0100

    feat(day 3): séparation en groupes

---
+++ Day 3 - Rucksack Reorganization/partie 2/rucksack.py
@@ -0,0 +1,53 @@
+import re
+
+
+class Rucksack:
+    def __init__(self, items: str):
+        self.first_compartment = items[: len(items) // 2]
+        self.second_compartment = items[len(items) // 2 :]
+
+    @property
+    def first_compartment(self) -> str:
+        return self._first_compartment
+
+    @first_compartment.setter
+    def first_compartment(self, value: str):
+        self._first_compartment = value
+
+    @property
+    def second_compartment(self) -> str:
+        return self._second_compartment
+
+    @second_compartment.setter
+    def second_compartment(self, value: str):
+        self._second_compartment = value
+
+    def find_duplicate(self) -> str:
+        return set(self.first_compartment).intersection(self.second_compartment).pop()
+
+    def get_priority(self) -> int:
+        duplicate = self.find_duplicate()
+        if ord("a") <= ord(duplicate) <= ord("z"):
+            return ord(duplicate) - ord("a") + 1
+        return ord(duplicate) - ord("A") + 27
+
+
+class RucksackManager:
+    def __init__(self, rucksacks_items: str):
+        self.rucksacks_items = rucksacks_items
+        rucksacks_list = rucksacks_items.splitlines()
+        self.rucksacks = map(Rucksack, rucksacks_list)
+
+    def get_total_priority(self) -> int:
+        return sum(map(Rucksack.get_priority, self.rucksacks))
+
+    def get_groups(self) -> list[str]:
+        groups = re.findall(r"(?:^.+$\n?){1,3}", self.rucksacks_items, re.MULTILINE)
+        return list(map(str.rstrip, groups))
+
+
+if __name__ == "__main__":
+    with open("../input.txt", "r") as file:
+        input_text = file.read()
+        my_rucksack_manager = RucksackManager(input_text)
+        print(f"The total priority is {my_rucksack_manager.get_total_priority()}.")
---
+++ Day 3 - Rucksack Reorganization/partie 2/test_rucksack.py
@@ -0,0 +1,88 @@
+from rucksack import Rucksack, RucksackManager
+
+
+def test_get_compartments_content():
+    my_rucksack = Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
+    assert my_rucksack.first_compartment == "vJrwpWtwJgWr"
+    assert my_rucksack.second_compartment == "hcsFMMfFFhFp"
+
+    my_rucksack = Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
+    assert my_rucksack.first_compartment == "jqHRNqRjqzjGDLGL"
+    assert my_rucksack.second_compartment == "rsFMfFZSrLrFZsSL"
+
+    my_rucksack = Rucksack("PmmdzqPrVvPwwTWBwg")
+    assert my_rucksack.first_compartment == "PmmdzqPrV"
+    assert my_rucksack.second_compartment == "vPwwTWBwg"
+
+
+def test_find_duplicate():
+    my_rucksack = Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
+    assert my_rucksack.find_duplicate() == "p"
+
+    my_rucksack = Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
+    assert my_rucksack.find_duplicate() == "L"
+
+    my_rucksack = Rucksack("PmmdzqPrVvPwwTWBwg")
+    assert my_rucksack.find_duplicate() == "P"
+
+    my_rucksack = Rucksack("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")
+    assert my_rucksack.find_duplicate() == "v"
+
+    my_rucksack = Rucksack("ttgJtRGJQctTZtZT")
+    assert my_rucksack.find_duplicate() == "t"
+
+    my_rucksack = Rucksack("CrZsJsPPZsGzwwsLwLmpwMDw")
+    assert my_rucksack.find_duplicate() == "s"
+
+
+def test_get_priority():
+    my_rucksack = Rucksack("vJrwpWtwJgWrhcsFMMfFFhFp")
+    assert my_rucksack.get_priority() == 16
+
+    my_rucksack = Rucksack("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL")
+    assert my_rucksack.get_priority() == 38
+
+    my_rucksack = Rucksack("PmmdzqPrVvPwwTWBwg")
+    assert my_rucksack.get_priority() == 42
+
+    my_rucksack = Rucksack("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn")
+    assert my_rucksack.get_priority() == 22
+
+    my_rucksack = Rucksack("ttgJtRGJQctTZtZT")
+    assert my_rucksack.get_priority() == 20
+
+    my_rucksack = Rucksack("CrZsJsPPZsGzwwsLwLmpwMDw")
+    assert my_rucksack.get_priority() == 19
+
+
+def test_get_total_priority():
+    my_rucksack_manager = RucksackManager(
+        """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+    )
+
+    assert my_rucksack_manager.get_total_priority() == 157
+
+
+def test_get_groups():
+    my_rucksack_manager = RucksackManager(
+        """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+    )
+
+    assert my_rucksack_manager.get_groups() == [
+        """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg""",
+        """wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw""",
+    ]
